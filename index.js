94f3731d1eca010386c4258552d567deaf82f709
Georgy Devyatkin <georgy.devyatkin@gmail.com>
2017-08-24 22:25:27 +0300

    fix sound

--- index.js
+++ index.js
@@ -326,21 +326,25 @@
          */
         loadSounds: function () {
             if (!IS_IOS) {
-                this.audioContext = new AudioContext();
+                try {
+                    this.audioContext = new AudioContext();
 
-                var resourceTemplate =
-                    document.getElementById(this.config.RESOURCE_TEMPLATE_ID).content;
+                    var resourceTemplate =
+                        document.getElementById(this.config.RESOURCE_TEMPLATE_ID).content;
 
-                for (var sound in Runner.sounds) {
-                    var soundSrc =
-                        resourceTemplate.getElementById(Runner.sounds[sound]).src;
-                    soundSrc = soundSrc.substr(soundSrc.indexOf(',') + 1);
-                    var buffer = decodeBase64ToArrayBuffer(soundSrc);
+                    for (var sound in Runner.sounds) {
+                        var soundSrc =
+                            resourceTemplate.getElementById(Runner.sounds[sound]).src;
+                        soundSrc = soundSrc.substr(soundSrc.indexOf(',') + 1);
+                        var buffer = decodeBase64ToArrayBuffer(soundSrc);
 
-                    // Async, so no guarantee of order in array.
-                    this.audioContext.decodeAudioData(buffer, function (index, audioData) {
-                        this.soundFx[index] = audioData;
-                    }.bind(this, sound));
+                        // Async, so no guarantee of order in array.
+                        this.audioContext.decodeAudioData(buffer, function (index, audioData) {
+                            this.soundFx[index] = audioData;
+                        }.bind(this, sound));
+                    }
+                } catch (e) {
+
                 }
             }
         },
